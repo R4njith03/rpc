b2fa23df7eb5b19e816c8f10432974c400db746d
R4njith03 <159175937+R4njith03@users.noreply.github.com>
2024-02-06 22:59:59 +0530

    Add files via upload

---
+++ add_server.c
@@ -0,0 +1,18 @@
+#include "add.h"
+#include <stdlib.h>
+
+int *
+
+add_1_svc(numbers *argp, struct svc_req *rqstp)
+
+{
+
+static int result;
+
+printf("add(%d,%d) is called\n", argp->a,argp->b);
+
+result = argp->a + argp->b;
+
+return &result;
+
+}
